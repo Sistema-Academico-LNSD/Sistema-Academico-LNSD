25204a2cbc59ce2ce57db484127f7b4f8c603cc8
Javier Mendez <88914048+JaviMendez88@users.noreply.github.com>
2026-09-22 18:16:04 -0600

    Push Web App
    TEST

---
+++ ProyectoLNSD_WApp/ProyectoLNSD_WApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoLNSD_WApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
