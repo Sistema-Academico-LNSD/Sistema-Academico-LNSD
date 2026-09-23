f1c89cf60c5eee61e9c328b2a649b34119b4ceb7
Javier Mendez <88914048+JaviMendez88@users.noreply.github.com>
2026-09-22 18:29:51 -0600

    Push 2
    TEST 2

---
+++ ProyectoLNSD_WApp/ProyectoLNSD_WApp.DAL/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+//using ProyectoLNSD_WApp.DAL.Entities;
+
+namespace ProyectoLNSD_WApp.DAL.Data
+{
+    public partial class AppDbContext : DbContext
+    {
+
+        public AppDbContext()
+        {
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
