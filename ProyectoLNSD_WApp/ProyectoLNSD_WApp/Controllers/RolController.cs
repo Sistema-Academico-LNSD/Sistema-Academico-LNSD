0c63fd095de32c450fa2577b0ef25f1ce503bffb
Javier Mendez <88914048+JaviMendez88@users.noreply.github.com>
2026-09-22 21:04:39 -0600

    Push Controller and Views
    Controllers and Views listos para MUF-01. Después, se continuará con completion de Historias de Usuario.

---
+++ ProyectoLNSD_WApp/ProyectoLNSD_WApp/Controllers/RolController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoLNSD_WApp.BLL.DTO;
+using ProyectoLNSD_WApp.BLL.Service.Rol;
+
+namespace ProyectoLNSD_WApp.Controllers
+{
+    public class RolController : Controller
+    {
+        private readonly IRolService _rolService;
+
+        public RolController(IRolService rolService)
+        {
+            _rolService = rolService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetRoles()
+        {
+            var respuesta =
+                await _rolService.GetRoles();
+
+            return Json(respuesta);
+        }
+
+        public async Task<IActionResult> GetRolById(int id)
+        {
+            var respuesta =
+                await _rolService.GetRolById(id);
+
+            return Json(respuesta);
+        }
+
+        public async Task<IActionResult> CreateRol(RolDTO rol)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var respuesta =
+                await _rolService.CreateRol(rol);
+
+            return Json(respuesta);
+        }
+
+        public async Task<IActionResult> UpdateRol(RolDTO rol)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var respuesta =
+                await _rolService.UpdateRol(rol);
+
+            return Json(respuesta);
+        }
+
+        public async Task<IActionResult> DeleteRol(int id)
+        {
+            var respuesta =
+                await _rolService.DeleteRol(id);
+
+            return Json(respuesta);
+        }
+    }
+}
---
+++ ProyectoLNSD_WApp/ProyectoLNSD_WApp/Controllers/UsuarioController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoLNSD_WApp.BLL.DTO;
+using ProyectoLNSD_WApp.BLL.DTO.UsuarioActions;
+using ProyectoLNSD_WApp.BLL.Service.Usuario;
+
+namespace ProyectoLNSD_WApp.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly IUsuarioService _usuarioService;
+
+        public UsuarioController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Index()
+        {
+            return View(new List<UsuarioDTO>());
+        }
+
+        public async Task<IActionResult> GetUsuarios()
+        {
+            var respuesta = await _usuarioService.GetUsuarios();
+
+            return Json(respuesta);
+        }
+
+        public async Task<IActionResult> GetUsuarioById(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var respuesta =
+                await _usuarioService.GetUsuarioById(id);
+
+            return Json(respuesta);
+        }
+
+        public async Task<IActionResult> BuscarUsuarios(
+            string? nombre,
+            string? correo,
+            int? idRol,
+            bool? estado)
+        {
+            var respuesta =
+                await _usuarioService.BuscarUsuarios(
+                    nombre,
+                    correo,
+                    idRol,
+                    estado);
+
+            return Json(respuesta);
+        }
+
+        public async Task<IActionResult> CreateUsuario(
+            UsuarioCrearDTO usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var respuesta =
+                await _usuarioService.CreateUsuario(usuario);
+
+            return Json(respuesta);
+        }
+
+        public async Task<IActionResult> UpdateUsuario(
+            UsuarioActualizarDTO usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var respuesta =
+                await _usuarioService.UpdateUsuario(usuario);
+
+            return Json(respuesta);
+        }
+
+        public async Task<IActionResult> DeleteUsuario(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var respuesta =
+                await _usuarioService.DeleteUsuario(id);
+
+            return Json(respuesta);
+        }
+
+        public async Task<IActionResult> CambiarEstado(
+            int id,
+            bool estado)
+        {
+            var respuesta =
+                await _usuarioService.CambiarEstado(id, estado);
+
+            return Json(respuesta);
+        }
+    }
+}
